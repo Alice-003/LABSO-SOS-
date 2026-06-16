9eed3296d8feebc142cf95a3247b233c548aac8b
Manuel <ennahli2004@gmail.com>
2026-06-16 20:33:55 +0200

    gestione interfaccia comandi Client

--- client/Client.java
+++ client/Client.java
@@ -1,9 +1,25 @@
 package client;
 
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
-        String IP = args[0];
-        String porta = args[1];
+        CommandHandler commandHandler = new CommandHandler();
+        Scanner scn = new Scanner(System.in);
+        String input;
 
+        if (args.length != 0) {
+            System.out.println("Errore input. Riavviare il programma!");
+        } else {
+            while (true) {
+                System.out.print("> ");
+                input = scn.nextLine();
+                System.out.println(commandHandler.gestioneComandi(input));
+                if (commandHandler.gestioneComandi(input) == 3) {
+                    break;
+                }
+            }
+            scn.close();
+        }
     }
 }
--- client/CommandHandler.java
+++ client/CommandHandler.java
@@ -1,5 +1,33 @@
 package client;
 
 public class CommandHandler {
+    public int gestioneComandi(String input) {
+        switch (input.split(" ")[0].toLowerCase()) {
+            case "listdata":
+                if (input.split(" ").length == 2) {
+                    if (input.split(" ")[1].toLowerCase().equals("local")) {
+                        return 1;
+                    } else if (input.split(" ")[1].toLowerCase().equals("remote")) {
+                        return 2;
+                    } else {
+                        return -1;
+                    }
+                }
+            case "quit":
+                return 3;
 
+            case "add":
+                return 4;
+
+            case "download":
+                return 5;
+
+            default:
+                return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
