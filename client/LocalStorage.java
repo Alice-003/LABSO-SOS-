4f66b84aa8c77cd4ea3b609c20ed55d6a96a9731
Manuel <ennahli2004@gmail.com>
2026-06-19 11:29:50 +0200

    Implementazione logica dell'id del nodo

--- client/LocalStorage.java
+++ client/LocalStorage.java
@@ -1,5 +1,38 @@
 package client;
 
+import java.io.*;
+import java.util.Random;
+
 public class LocalStorage {
+    public static String generaID(int lunghezza) {
+        Random rnd = new Random();
+        int numeroGenerato;
+        int min = 48;
+        int max = 57;
+        String ID = "";
+        for (int i = 0; i < lunghezza; i++) {
+            numeroGenerato = min + rnd.nextInt(max - min) + 1;
+            ID = ID + ((char) numeroGenerato);
+        }
+        return ID;
+    }
+
+    public static void fileID() {
+        try {
+            File fileID = new File("client/Files/fileID.txt");
+            if (!fileID.exists()) {
+                fileID.createNewFile();
+                FileWriter fw = new FileWriter(fileID);
+                BufferedWriter bw = new BufferedWriter(fw);
+                bw.write(generaID(5));
+                bw.close();
+                fw.close();
+
+            }
+        } catch (Exception er) {
+
+        }
+
+    }
 
 }
