e422cf0c5f07f611b26e9c9a9acec0816a0a90a0
Manuel <ennahli2004@gmail.com>
2026-08-04 19:25:08 +0200

    modifica definizione di RICHIESTA_REGISTRAZIONE_A_SERVE

--- client/protocol/Protocol.java
+++ client/protocol/Protocol.java
@@ -1,7 +1,7 @@
 package client.protocol;
 
 public class Protocol {
-    
+
     public static final String RICHIESTA_TEMPERATURA = "TEMPERATURA";
     public static final String RICHIESTA_PRESSIONE = "PRESSIONE";
     public static final String RICHIESTA_CONCENTRAZIONE = "CONCENTRAZIONE";
@@ -12,5 +12,10 @@ public class Protocol {
     public static final String RICHIESTA_RISORSA_DA_NODO = "RICHIESTA_RISORSA_DA_NODO";
     public static final String RICHIESTA_NODO_CON_RISORSA = "RICHIESTA_NODO_CON_RISORSA";
     public static final String DESTINATARIO_SERVER = "SERVER";
+    public static final String RICHIESTA_REGISTRAZIONE_A_SERVER = "REGISTER";
+    public static final String SEPARATORE = " ";
+    public static final String TUTTO_OK = "OK";
+    public static final String AGGIUNGI_RISORSA = "ADD";
+    public static final String REGISTRO_GIA_EFFETTUATO = "REGISTRO GIA EFFETTUATO";
 
 }
