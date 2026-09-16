cc341aa16ef49a571c3b2758b49c730707a60690
Giulia844 <giulia.pisi3@studio.unibo.it>
2026-09-16 12:40:01 +0200

    errore risolto

--- client/protocol/Protocol.java
+++ client/protocol/Protocol.java
@@ -15,7 +15,7 @@ public class Protocol {
     public static final String FINE_TRASMISSIONE_NODO_NODO = "END";
     public static final String RICHIESTA_REGISTRAZIONE_A_SERVER = "REGISTER";
     public static final String SEPARATORE = " ";
-    public static final String TUTTO_OK = "OKBenvenuto";
+    public static final String TUTTO_OK = "OK";
     public static final String AGGIUNGI_RISORSA = "ADD";
     public static final String REGISTRO_GIA_EFFETTUATO = "REGISTRO GIA EFFETTUATO";
     public static final String comandoDOWNLOAD = "DOWN";
